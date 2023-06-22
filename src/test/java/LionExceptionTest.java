337915f8ee87efd973fecb773bdb7355ed6e6bd4
Artem Seregin <vincelamarcarter@yandex.ru>
2023-06-22 20:12:55 +0300

    commit LionExceptionTest

--- src/test/java/LionExceptionTest.java
+++ src/test/java/LionExceptionTest.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.junit.Assert.assertTrue;
+
 @RunWith(MockitoJUnitRunner.class)
 public class LionExceptionTest {
 
@@ -12,11 +14,13 @@ public class LionExceptionTest {
     Feline feline;
     @Test
     public void lionConstructorThrowsException(){
+        boolean thrown = false;
         try {
             new Lion("Боевой протодракон", feline);
         }   catch (Exception ex) {
+            thrown = true;
             ex.printStackTrace();
         }
-
+        assertTrue(thrown);
     }
 }
